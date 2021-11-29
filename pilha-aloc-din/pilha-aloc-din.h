09ce99b58f2e3bc4819688edf65a2f4b6af62ea3
Daniel Lima <danielmlimag@gmail.com>
2021-11-29 09:33:02 -0300

    feat: add more libraries

---
+++ pilha-aloc-din/pilha-aloc-din.h
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int tp_item; 
+
+typedef struct tp_no_aux {   
+  tp_item info;   
+  struct tp_no_aux *prox;   
+} tp_no; 
+
+
+typedef struct {   
+  tp_no *topo;   
+} tp_pilha;
+
+
+tp_pilha *inicializa_pilha (){
+   tp_pilha *pilha=(tp_pilha*) malloc(sizeof(tp_pilha));   
+   pilha->topo = NULL;   
+   return pilha;
+}  
+
+int pilha_vazia (tp_pilha *pilha){   
+ if (pilha->topo == NULL) return 1;
+ return 0;
+}
+
+tp_no *aloca() {
+	tp_no* pt;
+	pt=(tp_no*) malloc(sizeof(tp_no));
+	return pt;
+}
+
+int push (tp_pilha *pilha, tp_item e){   
+  tp_no *novo;   
+  novo=aloca();
+  if (!novo) return 0;
+
+  novo->info = e;  
+  if ( pilha_vazia(pilha) ) { //Se for o primeiro elemento da lista
+     novo->prox = NULL;   
+     }  
+  else {
+     novo->prox = pilha->topo;   
+     }  
+  pilha->topo = novo; 
+  return 1;   
+}        
+
+
+int pop (tp_pilha *pilha, tp_item *e){   
+  tp_no *aux;
+
+  if (pilha_vazia(pilha)) return 0;
+  *e=pilha->topo->info;
+  aux=pilha->topo;
+  pilha->topo=pilha->topo->prox;
+  free(aux);
+  aux=NULL;
+  return 1;   
+}        
+
+int top (tp_pilha *pilha, tp_item *e){   
+  tp_no *aux;
+
+  if (pilha_vazia(pilha)) return 0;
+  *e=pilha->topo->info;
+  return 1;   
+}        
+
+
+
+tp_pilha *destroi_pilha(tp_pilha *pilha) {    
+	 tp_no *atu=pilha->topo, *aux;
+     tp_item e;
+	 while (atu != NULL)
+	       {
+			aux = atu->prox;
+            pop(pilha, &e);  
+			atu= aux; 
+            }
+	pilha->topo = NULL;
+	free(pilha);
+	return NULL;
+}
+
+
+
+void imprime_pilha(tp_pilha *pilha) {
+     tp_pilha *pilha_aux;
+     tp_item e;
+     //cria e inicializa uma pilha auxiliar
+	 pilha_aux=inicializa_pilha();
+ //    pilha_aux->topo = pilha->topo; //atribui o topo M de pilha para pilha_aux
+     
+	 tp_no *atu=pilha->topo;
+     
+	 while (!pilha_vazia(pilha))
+	       {
+            pop(pilha,&e);
+            printf("%d ", e);
+            push(pilha_aux, e);           
+            }
+
+ //    pilha->topo = pilha_aux->topo; //atribui o topo de pilha_aux para pilha
+	
+	while (!pilha_vazia(pilha_aux))
+	       {
+            pop(pilha_aux,&e);
+            push(pilha, e);           
+            }
+
+}
+
+
+
